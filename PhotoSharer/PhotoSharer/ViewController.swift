5d835653d2e515700eee1d366b158fd98e53817c
Serhii Ostrovetskyi <so@rollncode.com>
2019-08-23 11:03:27 +0300

    init project

---
+++ PhotoSharer/PhotoSharer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoSharer
+//
+//  Created by Serhii Ostrovetskyi on 8/23/19.
+//  Copyright © 2019 dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
